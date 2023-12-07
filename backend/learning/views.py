dd25e44c1694a321b0a24c7b2637b302f4d49b0a
goesoftwarePY <wpiela9@gmail.com>
2023-12-07 11:32:32 +0100

    before question builder enhancement

--- backend/learning/views.py
+++ backend/learning/views.py
@@ -24,10 +24,10 @@ class QuestionCategoryDifficultyLevel(APIView):
             isEasy = ClosedEndedQuestion.objects.filter(Q(question_category__question_category__icontains=qcat) & Q(is_hard=0)).exists()
             isEasyList.append(isEasy)
 
-        question_types = {"questionTypes":list(categories),
+        question_categories = {"questionCategories":list(categories),
                           "isEasy":isEasyList}
         
-        return Response({"types":question_types}, status=status.HTTP_200_OK)
+        return Response({"types":question_categories}, status=status.HTTP_200_OK)
 
 
         
