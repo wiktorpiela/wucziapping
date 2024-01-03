38cfebdbf3e35a050ade9db4d85e2a5f5ecc4ac7
goesoftwarePY <wpiela9@gmail.com>
2024-01-03 06:58:50 +0100

    before open ended questions added

--- backend/learning/views.py
+++ backend/learning/views.py
@@ -1,6 +1,3 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import generics
 from django.db.models import Q
 
