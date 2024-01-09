ec7dcf29c00e080b5316c090728303a6d00260e2
goesoftwarePY <wpiela9@gmail.com>
2024-01-09 06:53:12 +0100

    removed useless content from questions utils module

--- backend/question_builder/question_utils.py
+++ backend/question_builder/question_utils.py
@@ -1,52 +1,6 @@
 import pandas as pd
 import numpy as np
 
-def make_db_tables_from_df_closed_ended(df:pd.DataFrame):
-
-    # make category table and relation ----------
-    df['category']=df['question'].str.split().str[:3].str.join(" ")
-    categories=df['category'].drop_duplicates()\
-        .to_frame()\
-        .reset_index()\
-        .rename(columns={'index':'category_key'})
-    categories['category_key'] = np.arange(1,len(categories)+1)
-    df = pd.merge(df, categories, how='left', on='category').drop(columns='category')
-
-    # possible answers ------------------
-    cols = ['A','B','C','D','E','F','G','H','I']
-    possible_answers = df[cols]\
-        .reset_index()\
-        .rename(columns={'index':'possible_answers_key'})
-    possible_answers['possible_answers_key'] = possible_answers['possible_answers_key']+1
-    df.drop(columns=cols, inplace=True)
-    df=pd.concat([df, possible_answers['possible_answers_key']], axis=1)
-
-    # question text --------------------------
-    question_txt = df['question'].drop_duplicates()\
-        .reset_index()\
-        .rename(columns={'index':'question_txt'})
-    question_txt['question_txt'] = np.arange(1, len(question_txt)+1)
-    df=pd.merge(df, question_txt, how='left', on='question').drop(columns='question')
-
-    # correct answers ------------------------
-    correct_ans = df['correct_answer']\
-        .drop_duplicates()\
-        .to_frame()\
-        .reset_index()\
-        .rename(columns={'index':'correct_answer_key'})
-    correct_ans['correct_answer_key'] = np.arange(1, len(correct_ans)+1)
-    df=pd.merge(df, correct_ans, how='left', on='correct_answer').drop(columns='correct_answer')
-
-    # isMulti
-    is_multi_df = df['isMulti'].drop_duplicates().to_frame()\
-        .reset_index()\
-        .rename(columns={'index':'is_multi_key'})
-    is_multi_df['is_multi_key'] = np.arange(1,len(is_multi_df)+1)
-
-    df = pd.merge(df, is_multi_df, how='left', on='isMulti').drop(columns='isMulti')
-    
-    return df, categories, possible_answers, question_txt, correct_ans, is_multi_df
-
 class DataFrameToDatabaseTables:
 
     def __init__(self, inputDataFrame:pd.DataFrame):
